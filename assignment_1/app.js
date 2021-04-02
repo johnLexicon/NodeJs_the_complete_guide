1b9b2ee1582c66c61c4935609983d2c0aef28795
John Lundgren <john.lundgren.lexicon@gmail.com>
2021-04-02 12:25:58 +0200

    Assignment 1 complete.

---
+++ assignment_1/app.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const fs = require('fs');
+
+const requestListener = (req, res) => {
+  res.setHeader('content-type', 'text/html');
+  let templatePath = undefined;
+  switch (req.url) {
+    case '/':
+      templatePath = './templates/greetings.html';
+      break;
+    case '/users':
+      templatePath = './templates/users.html';
+      break;
+    case '/create-user':
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+
+      if (req.method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+          body.push(chunk);
+        });
+        req.on('end', () => {
+          const parsedBody = Buffer.concat(body).toString();
+          const userName = parsedBody.split('=')[1];
+          console.log(userName);
+        });
+      }
+      return res.end();
+    default:
+      res.write('Could not get: ' + req.url);
+      return res.end();
+  }
+
+  fs.readFile(templatePath, 'utf8', (err, data) => {
+    if (err) {
+      res.write(`ERROR: ${err.message}`);
+      return res.end();
+    }
+    res.write(data);
+    res.end();
+  });
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(9999);
